ed43207d45630d572635ba4e8f227a507dd3ca3c
Dineysd <dineysd@hotmail.com>
2023-07-29 15:29:23 +0000

    criando o banco e a conexao com typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,25 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { UserService } from './user/user.service';
 import { UserController } from './user/user.controller';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, 
+    ConfigModule.forRoot({
+    envFilePath: '.development.env',
+  }), 
+  TypeOrmModule.forRoot({
+    type: 'postgres',
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    password: process.env.DB_PASSWORD,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    entities: [`${__dirname}/**/*.entity{.js,.ts}`],
+    migrations: [`${__dirname}/migration/{.ts,*.js}`],
+    migrationsRun: true,
+  }),],
   controllers: [UserController],
   providers: [UserService],
 })
