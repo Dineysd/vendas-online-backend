6a79f0faee0023d383707e268b2fb84d49d6ef3c
Dineysd <dineysd@hotmail.com>
2023-08-05 20:29:02 +0000

    feat: insert admin create category

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,25 +1,28 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ValidationPipe, UsePipes } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 import { ReturnCategory } from './dto/return-category.dto';
 import { UserType } from '../user/enums/user-type.enum';
 import { Roles } from '../decorators/roles.decorator';
+import { Validate } from 'class-validator';
+
 @Roles(UserType.User, UserType.Admin)
 @Controller('category')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
-
+  @Roles(UserType.Admin)
+  @UsePipes(ValidationPipe)
   @Post()
   create(@Body() createCategoryDto: CreateCategoryDto)  {
     return this.categoryService.create(createCategoryDto);
   }
-
+  @Roles(UserType.Admin)
   @Get()
   findAll() : Promise<ReturnCategory[]>{
     return this.categoryService.findAll();
   }
-
+  
   @Get(':id')
   findOne(@Param('id') id: string) {
     return ;
