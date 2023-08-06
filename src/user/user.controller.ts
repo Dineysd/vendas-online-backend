b9a22bce7960066bf0315856dbfe8c2b034e4135
Dineysd <dineysd@hotmail.com>
2023-08-06 18:26:18 +0000

    feat: insert update password

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,16 +1,18 @@
-import { Body, Controller,  Get,  Param,  Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller,  Get,  Param,  Patch,  Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UserService } from './user.service';
 import { UserEntity } from './entities/user.entity';
 import { ReturnUserDto } from './dtos/return-user.dto';
 import { Roles } from '../decorators/roles.decorator';
 import { UserType } from './enums/user-type.enum';
+import { UserId } from 'src/decorators/user-id.decorator';
+import { UpdatePasswordDto } from './dtos/update-password.mock';
 
 @Controller('user')
 export class UserController {
     constructor(private readonly service: UserService){}
 
-    //@Roles(UserType.Root)
+    @Roles(UserType.Root)
     @Post('/admin')
     async createAdmin(@Body() createUser: CreateUserDto): Promise<UserEntity> {
       return this.service.createUser(createUser, UserType.Admin);
@@ -21,7 +23,15 @@ export class UserController {
     async createUser(@Body() dto: CreateUserDto): Promise<UserEntity>{
         return this.service.createUser({...dto})
     }
-    //@Roles(UserType.Admin, UserType.Root)
+
+    @Roles(UserType.Admin, UserType.Root)
+    @UsePipes(ValidationPipe)
+    @Patch()
+    async updatePassWord(@Body() dto: UpdatePasswordDto, @UserId() id: number): Promise<UserEntity>{
+        return this.service.updatePasswordUser(id, dto)
+    }
+
+    @Roles(UserType.Admin, UserType.Root)
     @Get()
     async getAll(): Promise<ReturnUserDto[]> {
         return this.service.getAllUser();
