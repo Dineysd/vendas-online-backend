4131968fce7efda888cbfc03edd31166f34cf32e
Dineysd <dineysd@hotmail.com>
2023-08-12 12:29:53 +0000

    feat: insert test in clear cart

--- src/cart/__tests__/cart.service.spec.ts
+++ src/cart/__tests__/cart.service.spec.ts
@@ -7,6 +7,7 @@ import { CartProductService } from '../../cart-product/cart-product.service';
 import { CartEntityMock } from '../__mocks__/cart.mock';
 import { ReturnDeleteMock } from '../../__mocks__/return-delete.mock';
 import { userEntityMock } from '../../user/__mocks__/user.mock';
+import { NotFoundException } from '@nestjs/common';
 
 describe('CartService', () => {
   let service: CartService;
@@ -46,7 +47,7 @@ describe('CartService', () => {
     expect(cartRepository).toBeDefined();
   });
 
-  it('should return delete result if delete cart', async () => {
+  it('should return delete result if clear cart', async () => {
     const spy = jest.spyOn(cartRepository,'save')
 
     const resultClear = await service.clearCart(userEntityMock.id)
@@ -54,4 +55,38 @@ describe('CartService', () => {
     expect(resultClear).toEqual(ReturnDeleteMock);
     expect(spy.mock.calls[0][0]).toEqual({...CartEntityMock, active: false});
   });
+
+  it('should return findOne undefined', async () => {
+    jest.spyOn(cartRepository, 'findOne').mockResolvedValue(undefined);
+
+    expect(service.clearCart(userEntityMock.id)).rejects.toThrowError(
+      NotFoundException,
+    );
+  })
+
+  it('should return cart in success (not send relations)', async () => {
+    const spy = jest.spyOn(cartRepository, 'findOne');
+    const cart = await service.findCartByUserId(userEntityMock.id);
+
+    expect(cart).toEqual(CartEntityMock);
+    expect(spy.mock.calls[0][0].relations).toEqual(undefined);
+  });
+
+  it('should return cart in sucess (send relations)', async () =>{
+    const spy = jest.spyOn(cartRepository, 'findOne');
+    const cart = await service.findCartByUserId(userEntityMock.id, true);
+
+    expect(cart).toEqual(CartEntityMock);
+    expect(spy.mock.calls[0][0].relations).toEqual({
+      cartProduct: {
+      product: true,
+    }},)
+  });
+
+  it('should return notFoundExcpetion in not found cart', async ()=> {
+    jest.spyOn(cartRepository,'findOne').mockResolvedValue(undefined)
+
+    expect(service.findCartByUserId(userEntityMock.id))
+    .rejects.toThrowError(NotFoundException)
+  })
 });
